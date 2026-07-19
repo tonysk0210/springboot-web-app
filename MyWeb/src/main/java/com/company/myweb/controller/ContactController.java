680e002c39f1eb2b797d00ac3bd8f772acadcf8d
Tony Shangkuan @ Desktop <tonyshangkuan@gmail.com>
2026-07-20 04:54:34 +0800

    update

--- MyWeb/src/main/java/com/company/myweb/controller/ContactController.java
+++ MyWeb/src/main/java/com/company/myweb/controller/ContactController.java
@@ -3,12 +3,14 @@ package com.company.myweb.controller;
 import com.company.myweb.model.Contact;
 import com.company.myweb.service.ContactService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static com.company.myweb.constant.ProjectConstant.ANSI_GREEN;
@@ -16,14 +18,10 @@ import static com.company.myweb.constant.ProjectConstant.ANSI_RESET;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class ContactController {
 
     private final ContactService contactService;
-
-    @Autowired
-    public ContactController(ContactService contactService) {
-        this.contactService = contactService;
-    }
 
     @GetMapping("/contact")
     public String contactPage(Model model) {
